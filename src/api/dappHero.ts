67c3a7a92c65af6468029067d14e1ce6edffb2a4
ADE <ecg.drake@gmail.com>
2020-04-20 12:46:58 -0400

    Added Dev DB to requests

--- src/api/dappHero.ts
+++ src/api/dappHero.ts
@@ -6,24 +6,23 @@ const axios = Axios.create({ headers: { 'content-type': 'application/json' } })
 const DEV_URL = 'https://dapphero-admin.bubbleapps.io/version-new-layout/api/1.1/wf/contracts'
 const PROD_URL = 'https://dapphero-admin.bubbleapps.io/api/1.1/wf/contracts'
 
-const BASE_URL = process.env.NODE_ENV === 'production' ? PROD_URL : DEV_URL
-
-const POST = 'post'
-const GET = 'get'
-const PUT = 'put'
-const DELETE = 'delete'
-
-export const getContractsByProjectKey = async (projectId, options = { dev: false }) => {
+export const getContractsByProjectKey = async (projectId): Promise<string> => {
   // logger.log(`projectId: ${projectId}`)
 
   const body = { projectId }
   try {
-    const axiosResponse = (await axios({
+    const axiosResponseProd = axios({
       method: 'post',
-      url: options.dev ? DEV_URL : PROD_URL,
+      url: PROD_URL,
       data: body,
-    }))
-    const responseData = axiosResponse.data.response.data
+    })
+    const axiosResponseDev = axios({
+      method: 'post',
+      url: DEV_URL,
+      data: body,
+    })
+    const responses = await Promise.all([ axiosResponseProd, axiosResponseDev ])
+    const responseData = responses[0]?.data?.response?.data?.length > 2 ? responses[0]?.data?.response?.data : responses[1]?.data?.response?.data
     return responseData
   } catch (err) {
     // logger.error('Error in dappHero api, getContractsByProjectKey', err)
