9dd8af0600249aac43e6df28cbf4d5cfce0b04cd
DrakeEvans <ecg.drake@gmail.com>
2020-05-14 17:39:07 -0400

    Change the shape of the object cached

--- src/api/dappHero.ts
+++ src/api/dappHero.ts
@@ -6,7 +6,7 @@ const axios = Axios.create({ headers: { 'content-type': 'application/json' } })
 const DEV_URL = 'https://dapphero-admin.bubbleapps.io/version-new-layout/api/1.1/wf/contracts'
 const PROD_URL = 'https://dapphero-admin.bubbleapps.io/api/1.1/wf/contracts'
 
-export const getContractsByProjectKey = async (projectId): Promise<string> => {
+export const getContractsByProjectKey = async (projectId): Promise<Record<string, any>> => {
   // logger.log(`projectId: ${projectId}`)
 
   const body = { projectId }
@@ -22,7 +22,7 @@ export const getContractsByProjectKey = async (projectId): Promise<string> => {
       data: body,
     })
     const responses = await Promise.all([ axiosResponseProd, axiosResponseDev ])
-    const responseData = responses[0]?.data?.response?.data?.length > 2 ? responses[0]?.data?.response?.data : responses[1]?.data?.response?.data
+    const responseData = responses[0]?.data?.response?.data?.length > 2 ? responses[0]?.data : responses[1]?.data
     return responseData
   } catch (err) {
     // logger.error('Error in dappHero api, getContractsByProjectKey', err)
